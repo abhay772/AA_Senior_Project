891059903a7c7576851c9543521638da853fd913
abhay772 <absolanki772@gmail.com>
2022-11-02 12:02:22 -0700

    Fix: AA.PMTOGO.Registration.Program.cs

--- Submissions/Milestone2/AA.PMTOGO.Registration/AA.PMTOGO.Registration/Program.cs
+++ Submissions/Milestone2/AA.PMTOGO.Registration/AA.PMTOGO.Registration/Program.cs
@@ -1,88 +1,84 @@
-﻿using Microsoft.EntityFrameworkCore;
-using RegistrationDataAccess.DataAccess;
+﻿using RegistrationDataAccess.DataAccess;
 using RegistrationDataAccess.Models;
 
-Console.Write("Username:");
-string username = Console.ReadLine();
-
-// TODO: Check if the user exists already
-var foundUser = context.Users.Find(username);
-if (foundUser == null)
+namespace AA.PMTOGO.Registration
 {
-    Console.Write("Email:");
-    string email = Console.ReadLine();
-
-    Console.Write("Password:");
-    string password = Console.ReadLine();
-
-    var usr = new User()
+    public class Program
     {
-        UserName = username,
-        Email = email,
-        Password = password
-    };
-
-    var entityentry = context.Users.Add(usr);
-    context.SaveChanges();
-
-    // TODO: Succesful Log up message
-    Console.WriteLine("You have sucessfully registered.");
-
-//MainMenu();
-
-//void MainMenu()
-//{
-//    using (var context = new UserContext())
-//    {
-//        string menu = "Options:\n(1) Sign Up\n(2)Empty the datastore\n(3) Exit\n";
-
-//        while (true)
-//        {
-//            Console.WriteLine(menu);
-//            Console.Write("Enter you choice:");
+        public static void Main(string[] args)
+        {
 
 
-//            switch (Console.ReadLine())
-//            {
-
-//                case "1":
-
-
-//                        return ;
-//                    }
-//                    Console.WriteLine("That username is taken.");
-//                    return ;
+            //using (var context = new UserContext())
+            //{
+            //    string menu = "\n\nOptions:\n(1) Sign Up\n(2) Empty the datastore\n(3) Exit\n";
+            //    bool Continue = true;
+            //    while (Continue)
+            //    {
+            //        Console.WriteLine(menu);
+            //        Console.Write("Enter you choice:");
 
 
-//                case "2":
+            //        switch (Console.ReadLine())
+            //        {
 
-//                    EmptyDataStore(context);
+            //            case "1":
+            //                Console.Write("Username:");
+            //                string username = Console.ReadLine();
 
-//                    return ;
+            //                // TODO: Check if the user exists already
+            //                var foundUser = context.Users.Find(username);
 
-//                case "3":
-//                    break;
-//                    return ;
-//            }
-//        }
+            //                if (foundUser == null)
+            //                {
+            //                    Console.Write("Email:");
+            //                    string email = Console.ReadLine();
 
-//        int choice = Convert.ToInt32(Console.ReadLine());
+            //                    Console.Write("Password:");
+            //                    string password = Console.ReadLine();
 
-//        Console.Write("Do you want to sign up:");
-//        string Continue = Console.ReadLine();
+            //                    var usr = new User()
+            //                    {
+            //                        UserName = username,
+            //                        Email = email,
+            //                        Password = password
+            //                    };
 
-//        foreach (var usr in context.Users.ToList())
-//        {
-//            Console.WriteLine("{0}\n{1}\n{2} ",usr.UserName, usr.Email, usr.Password);
-//        }
-//    }
+            //                    var entityentry = context.Users.Add(usr);
+            //                    context.SaveChanges();
 
-//    void EmptyDataStore(UserContext context)
-//    {
-//        foreach (var usr in context.Users)
-//        {
-//            context.Users.Remove(usr);
-//        }
-//    }
+            //                    // TODO: Succesful Log up message
+            //                    Console.WriteLine("You have sucessfully registered.");
 
-//}
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("That username is taken.");
+            //                }
+            //                break;
+
+            //            case "2":
+
+            //                EmptyDataStore(context);
+
+            //                break;
+
+            //            case "3":
+            //                Continue = false;
+            //                break;
+            //        }
+            //    }
+
+            //    void EmptyDataStore(UserContext context)
+            //    {
+            //        foreach (var usr in context.Users)
+            //        {
+            //            context.Users.Remove(usr);
+            //        }
+            //    }
+
+            //}
+
+        }
+    }
+}
