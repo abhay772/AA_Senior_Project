c90c94fe699dfd32f8e84cf6c31a1055c821c178
abhay772 <absolanki772@gmail.com>
2022-11-08 20:04:27 -0800

    ADD: sqlLogger
    stable version up until sqlLogger

---
+++ Submissions/Milestone2/AA.PMTOGO.Registration/AA.PMTOGO.RegistrationDAO/SqlRegistrationDAO.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA.PMTOGO.RegistrationDAO;
+
+public class SqlRegistrationDAO
+{
+    private readonly string _connectionString = @"Server=.\SQLEXPRESS;Database=AA.PMTOGO.Registration;Trusted_Connection=True;Encrypt=false";
+
+	public SqlRegistrationDAO()
+	{
+		
+	}
+
+	public bool AddUser(string email, string password, DateOnly dob)
+	{
+		using(var connection = new SqlConnection())
+
+		return false;
+	}
+
+}
--- Submissions/Milestone2/AA.PMTOGO.Registration/Loggin/SqlLogger.cs
+++ Submissions/Milestone2/AA.PMTOGO.Registration/Loggin/SqlLogger.cs
@@ -34,8 +34,8 @@ public class SqlLogger
             {
                 return true;
             }
-            return false;
         }
+        return false;
     }
 }
 
